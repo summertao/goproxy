21b4c1eec75c4b7784168ae053182d8586194cef
summertao <summertao@users.noreply.github.com>
2020-06-18 17:26:16 +0800

    Update start_second.sh

--- start_second.sh
+++ start_second.sh
@@ -2,4 +2,4 @@
 set -e
 
 cd ~/proxy_5.2
-./proxy http --daemon --forever -t tcp -p ":33080" -T tls -P "8.210.120.231:33080" -C proxy.crt -K proxy.key -F ./auth-file.txt
+./proxy http --daemon --forever -t tcp -p ":33080" -T tls -P "ip:port" -C proxy.crt -K proxy.key -F ./auth-file.txt
